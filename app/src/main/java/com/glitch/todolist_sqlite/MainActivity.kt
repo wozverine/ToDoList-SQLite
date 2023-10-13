2437d27b39985ef82ae8444a22828ef409bb6325
wozverine <wozverine@users.noreply.github.com>
2023-10-13 17:36:43 +0300

    start

--- app/src/main/java/com/glitch/todolist_sqlite/MainActivity.kt
+++ app/src/main/java/com/glitch/todolist_sqlite/MainActivity.kt
@@ -2,7 +2,9 @@ package com.glitch.todolist_sqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.glitch.todolist_sqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
